351203e327bcb8b2beefa1b6447f62106779bf5b
André Pessoa <andrebpessoa09@gmail.com>
2026-04-16 16:20:30 -0300

    feat: adjust auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,7 @@ export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
 		provider: "sqlite",
 	}),
+	trustedOrigins: ["http://localhost:3000", "http://127.0.0.1:3000"],
 	emailAndPassword: {
 		enabled: true,
 	},
